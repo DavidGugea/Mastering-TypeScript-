7e67ec1200294812cca0629e8b80439ac452c1e3
David Gugea <davidgugea@yahoo.com>
2022-08-25 10:36:20 +0200

    Finished Chapter 1

---
+++ Chapter1/dist/main.js
@@ -0,0 +1,5 @@
+"use strict";
+const myBoolean = true;
+const myNumber = 1234;
+const myStringArray = ['first', 'second', 'third'];
+//# sourceMappingURL=main.js.map
---
+++ Chapter1/src/main.ts
@@ -0,0 +1,3 @@
+const myBoolean: boolean = true;
+const myNumber: number = 1234;
+const myStringArray: string[] = ['first', 'second', 'third'];
